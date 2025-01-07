a806b43dab7bb8ba1527f96356b830d117927025
venkateshvanaboina <venkateshvanaboina95531998@gmail.com>
2025-01-07 17:32:58 +0530

    ep-04

--- App.js
+++ App.js
@@ -19,12 +19,41 @@ import ReactDOM from "react-dom/client";
     )
 }
 
+const Footer = () => {
+    return (
+        <div className="footer">
+            <div className="copy-right">
+                <h3 className="copy right">Copy right logo</h3>
+            </div>
+            <div className="nav-links">
+                <a>About US</a>
+                <a>Home</a>
+                <a>Contact US</a>
+                <a>Address</a>
+                <a>Email US</a>
+            </div>
+            <div className="address">
+                <h2>Office Address</h2>
+                <p>
+                    L v Nagar , street number- 6,
+                    Old Gajuwaka,
+                    Visakhapatnam,
+                    530026
+                </p>
+            </div>
+        </div>
+    )
+}
+
 const Cards = () => {
     return (
         <div className="cards">
-            <h3>Merdian Restaurant</h3>
-           
-
+            <img className="image-1" alt="image1" src="https://b.zmtcdn.com/data/pictures/7/91727/3fa5d44b6504b1c30df33efc442ffe3a_featured_v2.jpg"></img>
+            <h4>Merdian Restaurant</h4>
+            <h4>Hotel</h4>
+            <h4>4.4 stars</h4>
+            <h4>38min</h4>
+            
         </div>
     )
 }
@@ -36,7 +65,7 @@ const Body = () => {
                 SearchBar
             </div>
             <div className="card-container">
-            <Cards />
+            <Cards /><Cards /><Cards /><Cards /><Cards /><Cards /><Cards /><Cards /><Cards />
 
             </div>
 
@@ -49,6 +78,7 @@ const Applayout = () => {
     return <div className="app">
        <Header />
         <Body />
+        <Footer />
     </div>
 }
 
